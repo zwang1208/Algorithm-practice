ce12db742365f8dea2e4197f0ec4bf04197edc09
Zheng Wang <zwang1208@gmail.com>
2019-04-10 10:40:44 -0400

    1.2 check permutation

---
+++ CTCI/1. Arrays and Strings/1.2 check permutation/checkPermutation.js
@@ -0,0 +1,30 @@
+/**
+ * Given two strings, write a method to decide if one is a permutation of the other.
+ */
+
+const isPermutation = (str1, str2) => {
+    let hash = {}
+
+    for(let i of str1) {
+        if(hash.hasOwnProperty(i)) {
+            hash[i] += 1
+        }else {
+            hash[i] = 1
+        }
+    }
+
+    for(let j of str2) {
+        if(!hash.hasOwnProperty(j)) {
+            return false
+        }
+        if(hash[j] === 1) {
+            delete hash[j]
+        }else {
+            hash[j] -= 1
+        }
+    }
+
+    return Object.keys(hash).length === 0
+}
+
+console.log(isPermutation('1122334455667788', '9911223344556677'))
