749291fec633c0047aefb9eaef4f412950b01867
Zheng Wang <zwang1208@gmail.com>
2019-04-10 09:48:24 -0400

    1.1 sort

--- CTCI/1. Arrays and Strings/1.1 Is Unique/isUnique.js
+++ CTCI/1. Arrays and Strings/1.1 Is Unique/isUnique.js
@@ -10,4 +10,18 @@
 
 const isUnique = (s) => (s === [...new Set(s)].join(''))
 
+/**
+ * sort
+ */
 
+const isUnique = (s) => {
+    s.sort()
+
+    for(let i = 1; i < s.length; ++i) {
+        if(s[i] === s[i-1]) {
+            return false
+        }
+    }
+
+    return true
+}
