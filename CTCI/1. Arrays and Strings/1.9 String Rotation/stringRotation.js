6a9032001b4946fe87f0be0c7e9239ed4ad4b52a
Zheng Wang <zwang1208@gmail.com>
2019-04-14 22:06:04 -0400

    1.9

---
+++ CTCI/1. Arrays and Strings/1.9 String Rotation/stringRotation.js
@@ -0,0 +1,21 @@
+/**
+ * Assume you have amethod isSubstring which checks ifone word is asubstring of another. 
+ * Given two strings, s1 and s2, write code to check if s2 is a rotation of s1 using only one call to isSubstring 
+ * (e.g.,"waterbottle"is a rotation of"erbottlewat").
+ * 
+ * 
+ * @param {string} s1 
+ * @param {string} s2 
+ * @return {boolean}
+ */
+
+const isRotatedString = (s1, s2) => {
+    if(s1.length !== s2.length) {
+        return false
+    }
+    return isSubstring(s1+s1, s2)
+}
+
+const isSubstring = (str, substr) => {
+    return str.includes(substr)
+}
