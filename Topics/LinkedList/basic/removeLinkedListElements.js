19769352eef5f661bf82e929d93e21f087bf2530
Zheng Wang <zwang1208@gmail.com>
2019-04-18 23:49:09 -0400

    remove element in LL

--- Topics/LinkedList/basic/removeLinkedListElements.js
+++ Topics/LinkedList/basic/removeLinkedListElements.js
@@ -1,10 +1,9 @@
 /**
  * 
  * Remove all elements from a linked list of integers that have value val.
-    Example:
-
-    Input:  1->2->6->3->4->5->6, val = 6
-    Output: 1->2->3->4->5
+ * Example:
+ * Input:  1->2->6->3->4->5->6, val = 6
+ * Output: 1->2->3->4->5
  */
 /**
  * Definition for singly-linked list.
