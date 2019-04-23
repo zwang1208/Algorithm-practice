7fbcc1ac4aba811c93a49079e7d69c7e7c229ce9
Zheng Wang <zwang1208@gmail.com>
2019-04-22 22:21:30 -0400

    add comment

--- Topics/LinkedList/basic/mergeTwoSortedLists.js
+++ Topics/LinkedList/basic/mergeTwoSortedLists.js
@@ -31,6 +31,6 @@ var mergeTwoLists = function(l1, l2) {
         }
         temp = temp.next
     }
-    temp.next = l1 || l2; 
+    temp.next = l1 || l2; //any node left in l1 or l2
     return newHead.next    
 };
