974dc129f2b91c5b9e87d60c00003ae616b30a7f
Zheng Wang <zwang1208@gmail.com>
2019-05-18 11:35:35 -0400

    decode string

---
+++ Topics/Queue & Stack/basic/decodeString.js
@@ -0,0 +1,40 @@
+/**
+ * Given an encoded string, return it's decoded string.
+ * The encoding rule is: k[encoded_string], where the encoded_string inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+ * You may assume that the input string is always valid; No extra white spaces, square brackets are well-formed, etc.
+ * Furthermore, you may assume that the original data does not contain any digits and that digits are only for those repeat numbers, k. For example, there won't be input like 3a or 2[4].
+ * 
+ * s = "3[a]2[bc]", return "aaabcbc".
+ * s = "3[a2[c]]", return "accaccacc".
+ * s = "2[abc]3[cd]ef", return "abcabccdcdcdef".
+ */
+
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var decodeString = function(s) {
+    let stack = [];
+    let curNum = 0;
+    let res = '';
+    
+    for(let i of s) {
+        if(i === '[') {
+            stack.push(res);
+            stack.push(curNum);
+            res = '';
+            curNum = 0;
+        } else if(i === ']') {
+            let num = stack.pop();
+            let prev = stack.pop();
+            res = prev + res.repeat(parseInt(num));
+        } else if(parseInt(i) || i === '0') {
+            curNum = curNum * 10 + parseInt(i);
+        } else {
+            res += i; 
+        }  
+    }
+    
+    return res;
+};
+
