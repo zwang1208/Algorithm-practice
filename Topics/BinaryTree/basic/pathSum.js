354b63aac6eae56f1cbd90ccf505d82b77259105
Zheng Wang <zwang1208@gmail.com>
2019-05-26 22:58:29 -0400

    path sum

---
+++ Topics/BinaryTree/basic/pathSum.js
@@ -0,0 +1,26 @@
+/**
+ * Given a binary tree and a sum, determine if the tree has a root-to-leaf path such that adding up all the values along the path equals the given sum.
+ * Note: A leaf is a node with no children.
+ */
+
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val) {
+ *     this.val = val;
+ *     this.left = this.right = null;
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @param {number} sum
+ * @return {boolean}
+ */
+var hasPathSum = function(root, sum) {
+    if(!root) return false;
+    
+    if(!root.left && !root.right && root.val === sum) return true;
+    
+    sum -= root.val;
+    
+    return hasPathSum(root.left, sum) || hasPathSum(root.right, sum); 
+};
