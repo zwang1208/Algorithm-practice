94cd6d1dcd8bbb8005d468e9159aa33917725c66
Zheng Wang <zwang1208@gmail.com>
2019-06-04 10:31:42 -0400

    reverse string

---
+++ Topics/Recursion1/reverseString.js
@@ -0,0 +1,19 @@
+/**
+ * Write a function that reverses a string. The input string is given as an array of characters char[].
+ * Do not allocate extra space for another array, you must do this by modifying the input array in-place with O(1) extra memory.
+ * You may assume all the characters consist of printable ascii characters.
+ */
+
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    if(s.length === 0) {
+        return
+    }
+    let temp = s[0];
+    s.shift();
+    reverseString(s);
+    s.push(temp)
+};
